667c1d3c9639c83155338fc493379ab69cc72eca
thenuboflegend <thenuboflegend@proton.me>
2024-11-19 09:34:51 -0600

    fix base error

--- dot_config/hypr/scripts/executable_paper.sh
+++ dot_config/hypr/scripts/executable_paper.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-declare CURR=$(( $(date +%H) / 3 % 7 + 1 ))
+declare CURR=$(( $(date +%k) / 3 % 7 + 1 ))
 hyprctl hyprpaper preload "~/.config/hypr/paper/${CURR}.jpg"
 hyprctl hyprpaper wallpaper "HDMI-A-1,~/.config/hypr/paper/${CURR}.jpg"
 
 while [ true ]; do
-  if [ $(( $(date +%H) / 3 % 7 + 1 )) -ne ${CURR} ]; then
-    hyprctl hyprpaper preload "~/.config/hypr/paper/$(( $(date +%H) / 3 % 7 + 1 )).jpg"
-    hyprctl hyprpaper wallpaper "HDMI-A-1,~/.config/hypr/paper/$(( $(date +%H) / 3 % 7 + 1 )).jpg"
+  if [ $(( $(date +%k) / 3 % 7 + 1 )) -ne ${CURR} ]; then
+    hyprctl hyprpaper preload "~/.config/hypr/paper/$(( $(date +%k) / 3 % 7 + 1 )).jpg"
+    hyprctl hyprpaper wallpaper "HDMI-A-1,~/.config/hypr/paper/$(( $(date +%k) / 3 % 7 + 1 )).jpg"
     hyprctl hyprpaper unload "~/.config/hypr/paper/${CURR}.jpg"
   fi
-  CURR=$(( $(date +%H) / 3 % 7 + 1 ))
+  CURR=$(( $(date +%k) / 3 % 7 + 1 ))
   sleep 900
 done
