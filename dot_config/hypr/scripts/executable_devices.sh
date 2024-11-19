4cf9dfab80445230fb50b544c98d6b7eed8baa37
thenuboflegend <thenuboflegend@proton.me>
2024-11-19 14:32:24 -0600

    cont devices

--- dot_config/hypr/scripts/executable_devices.sh
+++ dot_config/hypr/scripts/executable_devices.sh
@@ -7,6 +7,10 @@ monitor=eDP-1,1920x1080,auto,1
 bind = , XF86MonBrightnessUp, exec, brightnessctl set +25
 bind = , XF86MonBrightnessDown, exec, brightnessctl set 25-
 
+# SUSPEND
+bindl=,switch:on:Lid Switch, exec, hyprctl dispatch dpms off && hyprctl dispatch exec hyprlock
+bindl=,switch:off:Lid Switch, exec, hyprctl dispatch dpms on
+
 input {
   sensitivity = -0.3
 }" > ../devices.conf
@@ -14,6 +18,9 @@ input {
 elif [[ $(cat /etc/hostname) == "nublap" ]]; then
   echo "
 monitor=,2560x1440@144,auto,auto
+
+# SUSPEND
+exec-once = ~/.config/hypr/scripts/suspend.sh
 
 env = LIBVA_DRIVER_NAME,nvidia
 env = XDG_SESSION_TYPE,wayland
--- dot_config/hypr/scripts/executable_paper.sh
+++ dot_config/hypr/scripts/executable_paper.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 declare CURR=$(( $(date +%k) / 3 % 7 + 1 ))
+declare MON
+if [[ $(cat /etc/hostname) == "nublap" ]]; then
+  MON="eDP-1"
+elif [[ $(cat /etc/hostname) == "nublap" ]]; then
+  MON="HDMI-A-1"
+fi
 hyprctl hyprpaper preload "~/.config/hypr/paper/${CURR}.jpg"
-hyprctl hyprpaper wallpaper "HDMI-A-1,~/.config/hypr/paper/${CURR}.jpg"
+hyprctl hyprpaper wallpaper "${MON},~/.config/hypr/paper/${CURR}.jpg"
 
 while [ true ]; do
   if [ $(( $(date +%k) / 3 % 7 + 1 )) -ne ${CURR} ]; then
