186f916b319bebef3bc96849f1bbf1798c0a923f
thenuboflegend <thenuboflegend@proton.me>
2024-11-25 14:40:46 -0600

    rm swayidle

--- dot_config/hypr/scripts/executable_suspend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swayidle -w \
-  timeout 600 ' hyprlock' \
-  timeout 900 ' hyprctl dispatch dpms off' \
-  timeout 960 'systemctl suspend' \
-    resume ' hyprctl dispatch dpms on' \
-  before-sleep ' hyprlock'
