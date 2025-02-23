3a7ae193d0576e3af1b43fa5ca3ed91b42485953
thenuboflegend <thenuboflegend@proton.me>
2025-02-23 15:10:47 -0600

    nvim and foot to gruv

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,12 @@
 return {
-  'sainnhe/everforest',
+  -- 'sainnhe/everforest',
+  'sainnhe/gruvbox-material',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.g.everforest_background = 'hard'
-    vim.cmd.colorscheme 'everforest'
+    -- vim.g.everforest_background = 'hard'
+    -- vim.cmd.colorscheme 'everforest'
+
+    vim.cmd.colorscheme('gruvbox-material')
 
     -- You can configure highlights by doing something like:
     -- vim.cmd.hi 'Comment gui=none'
