1509140d4b37b11901dc0b2b350d4bba39326752
thenuboflegend <thenuboflegend@proton.me>
2025-10-22 18:08:38 -0500

    rebase nvim

---
+++ dot_config/nvim/lua/plugins/fff.lua
@@ -0,0 +1,18 @@
+return {
+  'dmtrKovalenko/fff.nvim',
+  build = 'cargo build --release',
+  -- or if you are using nixos
+  -- build = "nix run .#release",
+  opts = {
+    prompt = '> ',
+  },
+  keys = {
+    {
+      '<leader>f', -- try it if you didn't it is a banger keybinding for a picker
+      function()
+        require('fff').find_files() -- or find_in_git_root() if you only want git files
+      end,
+      desc = 'Open file picker',
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/mini_indentscope.lua
@@ -0,0 +1,12 @@
+return {
+  'echasnovski/mini.indentscope',
+  opts = {
+    draw = {
+      delay = 0,
+      animation = function()
+        return 0
+      end,
+    },
+    symbol = '│',
+  },
+}
